e40b9c63ac7a2742dc545de2dadd3ee77ddfe23a
jy97 <jun.yoyoyoshida@gmail.com>
2014-01-18 04:34:44 +0100

    added intro animation. added fixed header.

--- js/index.js
+++ js/index.js
@@ -2,12 +2,52 @@
 
 
 $(function(){
- 
+$("#header").hide();
 $("#main").hide();
-main();
+
+
+var current_frame, total_frames, path, length, handle, myobj;
+myobj = document.getElementById('logo').cloneNode(true);
+
+var init = function() {
+  current_frame = 0;
+  total_frames = 60;
+  path = new Array();
+  length = new Array();
+  for(var i=0; i<3;i++){
+    path[i] = document.getElementById('i'+i);
+    l = path[i].getTotalLength();
+    length[i] = l;
+    path[i].style.strokeDasharray = l + ' ' + l; 
+    path[i].style.strokeDashoffset = l;
+  }
+  handle = 0;
+}
+var draw = function() {
+   var progress = current_frame/total_frames;
+   if (progress > 1) {
+     window.cancelAnimationFrame(handle);
+   } else {
+     current_frame++;
+     for(var j=0; j<path.length;j++){
+		 path[j].getBoundingClientRect();
+		 path[j].style.transition = path[j].style.WebkitTransition =  '1s ease-out';
+	     path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
+     }
+     handle = window.requestAnimationFrame(draw);
+   }
+};
 	
-	function main () {
-		$("#main").fadeIn();
-		$('#main').nested({minWidth: 100, gutter: 1, animate: true});
-	}
+init();
+draw();
+
+	setTimeout(function(){
+		$("#logo").animate({ 'top': '-2px', 'left': '12px', width: '70px', height: '70px', opacity: '0.2'},{duration: 800, easing: "swing"});
+		$("#logo").fadeOut("fast",function(){
+			$("#main").fadeIn();
+		});
+		$("#header").fadeIn(2500);
+	}, 3200);
+
+
 });
