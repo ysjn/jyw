57a24a9e56cd60c8e3bf5d9fcb6d4050d4f1a2f9
jy97 <jun.yoyoyoshida@gmail.com>
2014-01-18 02:11:08 +0100

    removed somestuff - see comments
    removed :
    entrance
    nested.js
    imac.png

--- js/index.js
+++ js/index.js
@@ -3,24 +3,11 @@
 
 $(function(){
  
-$('body').addClass('noscroll');
 $("#main").hide();
-$("#entrance").hide();
-$("#entrance").fadeIn(1000);
-	
-	$("#entrance").click(function(e){
-		$( "#hello" ).effect( "pulsate", "slow");
-		$("#hello").fadeOut(entrance);
-	});
-	
-	function entrance () {
-		$("#entrance").fadeOut(main);
-		$(".wrapbox").fadeOut();
-	}
+main();
 	
 	function main () {
 		$("#main").fadeIn();
 		$('#main').nested({minWidth: 100, gutter: 1, animate: true});
-		$('body').removeClass('noscroll');	
 	}
 });
