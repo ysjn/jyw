2fe829c08a99d0750d9ce995c124a25abed63366
ysjn <jun.yoyoyoshida@gmail.com>
2022-06-11 23:11:19 +0900

    feat: play intro animation only when initial visit or logo click

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,49 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import styles from './index.module.scss';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import { Footer } from '@/components/Footer/Footer';
 import { Logo } from '@/components/Logo/Logo';
 import { Nav } from '@/components/Nav/Nav';
+import { HAS_VISITED } from 'lib/constants';
+import { LocalStorageEngine } from 'lib/localStorageEngine';
 
 const Home: NextPage = () => {
   const [introComplete, setIntroComplete] = useState(false);
-  useEffect(() => {
-    setTimeout(() => setIntroComplete(true), 3000);
-  }, []);
+
+  useLayoutEffect(() => {
+    const LS = new LocalStorageEngine();
+    if (LS.load(HAS_VISITED)) {
+      setIntroComplete(true);
+      return;
+    }
+
+    setTimeout(() => {
+      setIntroComplete(true);
+      LS.save(HAS_VISITED, '1');
+    }, 4000);
+  }, [setIntroComplete]);
 
   return (
-    <div>
+    <div className={!introComplete ? styles.Root__introPlaying : undefined}>
       <Head>
         <title>Jy. Portfolio</title>
       </Head>
-      <Logo />
-      <Nav isHome />
+      {!introComplete && <Logo />}
+      <Nav noAnimation={introComplete} isHome />
 
       <div id="wrapper">
         <div id="main">
           <Carousel
             swipeable={false}
             showStatus={false}
+            showThumbs={false}
             autoPlay={introComplete}
             showArrows
             infiniteLoop
