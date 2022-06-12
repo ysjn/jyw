24241cbe7fc883d3e61252d6f04bd8502473f83e
ysjn <jun.yoyoyoshida@gmail.com>
2022-06-12 23:10:45 +0900

    feat: added feature to preview image in lightbox

---
+++ modules/WorksContents.tsx
@@ -0,0 +1,86 @@
+import FsLightBox from 'fslightBox-react'; // eslint-disable-line import/no-unresolved
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { Back } from '@/components/Back/Back';
+import { Footer } from '@/components/Footer/Footer';
+import { Grid } from '@/components/Grid/Grid';
+import { GridItem } from '@/components/Grid/GridItem';
+import { Nav } from '@/components/Nav/Nav';
+import { WORKS } from 'lib/constants';
+
+interface WorksContentsProps {
+  id: string;
+  children?: React.ReactNode;
+}
+export const WorksContents: React.FC<WorksContentsProps> = (props) => {
+  const [lightBoxControl, setLightBoxControl] = useState({
+    isOpen: false,
+    currentIndex: 1,
+  });
+
+  const openLightBox = (number: number) => {
+    setLightBoxControl({
+      isOpen: !lightBoxControl.isOpen,
+      currentIndex: number,
+    });
+  };
+
+  const data = WORKS.find((work) => work.id === props.id);
+  if (!data) {
+    return null;
+  }
+  const lightBoxImages = data.gallery?.map((image) => image.src) || [];
+  const lightBoxAlts = data.gallery?.map((image) => ({ alt: image.alt })) || [];
+  return (
+    <div>
+      {data.title && (
+        <Head>
+          <title>{`Works. ${data.title}`}</title>
+        </Head>
+      )}
+      <Nav />
+      <div className="Wrapper">
+        <div className="Main">
+          <Back />
+          <Grid column={2} gap={16} vAlign="start" noBackground noBorder>
+            {data.gallery && (
+              <GridItem>
+                <Grid column={2}>
+                  {data.gallery.map((image, index) => (
+                    <GridItem key={index}>
+                      <div onClick={() => openLightBox(index + 1)} style={{ cursor: 'pointer' }}>
+                        <Image src={image.thSrc} width="240" height="150" alt={image.alt} />
+                      </div>
+                    </GridItem>
+                  ))}
+                </Grid>
+              </GridItem>
+            )}
+            <GridItem>
+              <section className="util-MarginTop--16">
+                <h1 className="util-MarginBottom--32">{data.title}</h1>
+                <p className="util-MarginBottom--16">{data.descJp}</p>
+                <p className="util-MarginBottom--16">{data.descEn}</p>
+                {data.siteUrl && (
+                  <Link href={data.siteUrl} target="_blank">
+                    サイトを開く / Launch site
+                  </Link>
+                )}
+              </section>
+            </GridItem>
+          </Grid>
+          {props.children}
+        </div>
+        <FsLightBox
+          toggler={lightBoxControl.isOpen}
+          sources={lightBoxImages}
+          customAttributes={lightBoxAlts}
+          slide={lightBoxControl.currentIndex}
+        />
+        <Footer />
+      </div>
+    </div>
+  );
+};
--- pages/works/blocks.tsx
+++ pages/works/blocks.tsx
@@ -1,73 +1,16 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Back } from '@/components/Back/Back';
-import { Footer } from '@/components/Footer/Footer';
-import { Grid } from '@/components/Grid/Grid';
-import { GridItem } from '@/components/Grid/GridItem';
-import { Nav } from '@/components/Nav/Nav';
-import { WORKS } from 'lib/constants';
+import { WorksContents } from '@/modules/WorksContents';
 
-const works_blocks: NextPage = () => {
-  const id = 'blks';
-  const data = WORKS.find((work) => work.id === id);
-  if (!data) {
-    return null;
-  }
-  return (
-    <div>
-      {data.title && (
-        <Head>
-          <title>{`Works. ${data.title}`}</title>
-        </Head>
-      )}
-      <Nav />
-      <div className="Wrapper">
-        <div className="Main">
-          <Back />
-          <Grid column={2} gap={16} vAlign="start" noBackground noBorder>
-            {data.gallery && (
-              <GridItem>
-                <Grid column={2}>
-                  {data.gallery.map((item, index) => (
-                    <GridItem key={index}>
-                      <Link href={item.src}>
-                        <a>
-                          <Image src={item.thSrc} width="240" height="150" alt="" />
-                        </a>
-                      </Link>
-                    </GridItem>
-                  ))}
-                </Grid>
-              </GridItem>
-            )}
-            <GridItem>
-              <section className="util-MarginTop--16">
-                <h1 className="util-MarginBottom--32">{data.title}</h1>
-                <p className="util-MarginBottom--16">{data.descJp}</p>
-                <p className="util-MarginBottom--16">{data.descEn}</p>
-                {data.siteUrl && (
-                  <Link href={data.siteUrl} target="_blank">
-                    サイトを開く / Launch site
-                  </Link>
-                )}
-              </section>
-            </GridItem>
-          </Grid>
-
-          <div className="Video">
-            <iframe
-              src="//player.vimeo.com/video/96424638?portrait=0&title=0&badge=0&byline=0"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </div>
-      </div>
-      <Footer />
+const works_blocks: NextPage = () => (
+  <WorksContents id="blks">
+    <div className="Video">
+      <iframe
+        src="//player.vimeo.com/video/96424638?portrait=0&title=0&badge=0&byline=0"
+        frameBorder="0"
+        allowFullScreen
+      />
     </div>
-  );
-};
+  </WorksContents>
+);
 
 export default works_blocks;
