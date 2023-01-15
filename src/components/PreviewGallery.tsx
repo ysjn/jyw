df3a08da1999ec5d01a8d8808b93672e58988481
ysjn <jun.yoyoyoshida@gmail.com>
2023-01-15 15:01:43 +0900

    add zoom function to preview

--- src/components/PreviewGallery.tsx
+++ src/components/PreviewGallery.tsx
@@ -1,6 +1,8 @@
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Grid } from '@components/Grid/Grid';
 import { GridItem } from '@components/Grid/GridItem';
+import { ZoomIn } from '@components/Icon/ZoomIn';
+import { ZoomOut } from '@components/Icon/ZoomOut';
 
 interface GalleryItem {
   src: string;
@@ -14,7 +16,19 @@ interface PreviewGalleryProps {
 
 export const PreviewGallery: React.FC<PreviewGalleryProps> = (props) => (
   <Grid column={2}>
-    <PhotoProvider maskOpacity={0.95}>
+    <PhotoProvider
+      maskOpacity={0.95}
+      toolbarRender={({ onScale, scale }) => (
+        <>
+          <div className="PhotoView-Slider__toolbarIcon" onClick={() => onScale(scale + 1)}>
+            <ZoomIn />
+          </div>
+          <div className="PhotoView-Slider__toolbarIcon" onClick={() => onScale(scale - 1)}>
+            <ZoomOut />
+          </div>
+        </>
+      )}
+    >
       {props.gallery.map((image: GalleryItem, index) => (
         <GridItem key={index}>
           <PhotoView src={image.src}>
